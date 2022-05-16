287f58b11b600d8d4e0d61a65012159bee9d4519
kalebe <kalebeadv66@gmail.com>
2022-05-16 15:21:12 -0300

    refatorando um pouco o  SaveSearch

--- SaveSearch.py
+++ SaveSearch.py
@@ -4,7 +4,7 @@ import os
 import glob
 import io
 
-def list_files_docs_in(path,docs):
+def list_files_docs_in(path):
 
     os.chdir(path[0])
     docs = glob.glob("*.*")
@@ -29,3 +29,26 @@ def verify_if_file_is_docx(file):
         return True
     else: 
         return False
+
+def search_in_doc(path, search):
+    files = list_files_docs_in(path)
+    arq = get_cache(search,path[2])
+    
+
+def get_cache(search,path):
+    cachelog = save_lines(path)
+    cachelog = return_file_cache(search,cachelog)
+    return cachelog
+
+def save_lines(path):
+    cachelog = io.open(path + "logSeach.txt", "r", encoding="utf-8")
+    cachelog = cachelog.readlines()
+    cachelog.close()
+    return cachelog
+
+def return_file_cache(search,cachelog):
+    for i in range(len(cachelog)):
+        searchArq = cachelog[i].split(":")
+        if searchArq[0] == search:
+            return searchArq[1].replace('\n', '')
+    return ""
