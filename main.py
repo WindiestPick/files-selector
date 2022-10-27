42f075c8f0b41943c6bd9713a0672ee3f1eece68
Kalebe <kalebeadv66@gmail.com>
2022-10-27 14:48:09 -0500

    Corrigi um pequeno bug na validação de CPF, adicionei o 0 '0' a validação

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ class Application():
             return b
 
     def validaCPF(self, a):
-        alfa = ['1','2','3','4','5','6','7','8','9','-','.']
+        alfa = ['0','1','2','3','4','5','6','7','8','9','-','.']
         for i in a:
             f = 0
             for j in alfa:
